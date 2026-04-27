83e59068718a239bc8f61c65a97f5f12ff8891b4
jams-mc <232049828+TheRedMineword@users.noreply.github.com>
2026-04-27 17:53:53 +0200

    Hot fix

--- cards.js
+++ cards.js
@@ -14,6 +14,31 @@ async function loadCards() {
     loadGwent();
 }
 
+
+// Websocket and Server config.
+let wsUrl;
+
+const host = window.location.hostname;
+
+if (
+  host.includes("localhost") ||
+  host.includes("127.0.0.1") ||
+  host.includes("::1")
+) {
+  wsUrl = "ws://localhost:8081";
+} else {
+  wsUrl = `ws://${host}`;
+}
+
+console.log("Websocket", wsUrl);
+// const socket = new WebSocket('ws://127.0.0.1:8080');				// Example line for when using local installation instead of remote deployment.
+const socket = new WebSocket(wsUrl);	// Websocket + server is expected to be reachable on this URL. Disable if using local installation.
+let amReady = false;
+let opponentReady = false;
+let playerId = null;
+
+
+
 function loadGwent() {
     const script = document.createElement("script");
     script.src = "gwent.js";
--- gwent.js
+++ gwent.js
@@ -2,28 +2,6 @@
 
 class Controller {}
 
-// Websocket and Server config.
-let wsUrl;
-
-const host = window.location.hostname;
-
-if (
-  host.includes("localhost") ||
-  host.includes("127.0.0.1") ||
-  host.includes("::1")
-) {
-  wsUrl = "ws://localhost:8081";
-} else {
-  wsUrl = `ws://${host}`;
-}
-
-console.log("Websocket", wsUrl);
-// const socket = new WebSocket('ws://127.0.0.1:8080');				// Example line for when using local installation instead of remote deployment.
-const socket = new WebSocket(wsUrl);	// Websocket + server is expected to be reachable on this URL. Disable if using local installation.
-let amReady = false;
-let opponentReady = false;
-let playerId = null;
-
 // Better menu //
 const menuBtn = document.getElementById("top-menu-btn");
 const menu = document.getElementById("top-menu");
