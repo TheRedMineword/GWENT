53bd0952630e9c394a6457f03dbefde54834c2fd
TheRedMineword <hsmineword2@int.pl>
2026-06-16 17:41:11 +0200

    Updated 404.html

--- change/appinfo/commits_gen/run.js
+++ change/appinfo/commits_gen/run.js
@@ -35,7 +35,8 @@ const IGNORE_FILES = new Set([
     '.init.bat', '.replace.env', 'server.js', '.server.js',
     '.env',
     'session_registering_oryginal.js', 'invite.json', 'package.json', 'package-lock.json', 'index_ver_check_script.txt',
-    'allowed_versions.json', 'convert.js', 'audio-bin-config.json', '.audio-bin-config.json', 'audio-bin-config.bin', '.audio-bin-config.bin'
+    'allowed_versions.json', 'convert.js', 'audio-bin-config.json', '.audio-bin-config.json', 'audio-bin-config.bin', '.audio-bin-config.bin',
+    '404.html'
 ]);
 
 // =====================================================
