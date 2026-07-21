f40c29300cf8d9eaed260780ab1c12b4a7d1fd57
TheRedMineword <232049828+TheRedMineword@users.noreply.github.com>
2026-07-21 13:17:20 +0200

    appinfo

--- javascript/gwent.js
+++ javascript/gwent.js
@@ -841,12 +841,10 @@ socket.onmessage = async (event) => {
       }
       break;
     case "server_notif":
-    try {
+      try {
         pushMessage(formatMessage2(data.msg), data.duration);
-    } catch (e){
-
-    }
-    break;
+      } catch (e) {}
+      break;
     case "authRequired":
       ip_data = data._ip;
       console.log("[IP PARSE]", data._ip);
